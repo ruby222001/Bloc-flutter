bbdc98b2ad7c417264d84e3f60019fb1fd0b89ba
rubi maharjan <rubymaharzn@gmail..com>
2025-06-12 08:40:36 +0545

    first commit

---
+++ lib/counter_page.dart
@@ -0,0 +1,28 @@
+import 'package:bloc_prac/counter_cubit.dart';
+import 'package:bloc_prac/counter_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+//responsible for providing controller to view
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterCubit(0),
+      child: BlocListener<CounterCubit, int>(
+        listener: (context, state) {
+          if (state == 10) {
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      content: Text('ssf'),
+                    ));
+          }
+        },
+        child: CounterView(),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bloc_prac/counter_cubit.dart';
+import 'package:bloc_prac/counter_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  CounterPage(),
+    );
+  }
+}
